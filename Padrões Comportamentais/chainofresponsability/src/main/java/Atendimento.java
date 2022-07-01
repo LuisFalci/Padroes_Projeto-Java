530ee51e9670b975bca13049d9940ead66e3004c
Luis Gustavo Furtuozo Falci <78694561+LuisFalci@users.noreply.github.com>
2022-06-30 23:28:17 -0300

    Update Atendimento.java

--- Padrões Comportamentais/chainofresponsability/src/main/java/Atendimento.java
+++ Padrões Comportamentais/chainofresponsability/src/main/java/Atendimento.java
@@ -11,7 +11,6 @@ public class Atendimento {
     public TipoAtendimento getTipoAtendimento() {
         return tipoAtendimento;
     }
-    
 
     public void setTipoAtendimento(TipoAtendimento tipoAtendimento) {
         this.tipoAtendimento = tipoAtendimento;
