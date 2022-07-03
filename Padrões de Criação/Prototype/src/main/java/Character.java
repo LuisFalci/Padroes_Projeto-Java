bc8626a27de83699b50d1920399664c262732980
Luis Gustavo Furtuozo Falci <78694561+LuisFalci@users.noreply.github.com>
2022-07-02 21:54:05 -0300

    Update Character.java

--- Padrões de Criação/Prototype/src/main/java/Character.java
+++ Padrões de Criação/Prototype/src/main/java/Character.java
@@ -7,6 +7,7 @@ public class Character implements Cloneable {
     private double weight;
     
     
+    
 
     
     public Character(String body, Hat hat, String skin, int age, double height, double weight){
